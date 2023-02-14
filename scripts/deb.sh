36566ba98da19216ec8977fa38d6b005f1454d24
Christopher Pickering <christopher@going.bg>
2023-02-14 08:31:10 -0600

    updated install script

--- scripts/deb.sh
+++ scripts/deb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -s --compressed "https://packages.atlas.bi/deb/KEY.gpg" | apt-key add -
+curl -s --compressed "https://packages.atlas.bi/deb/KEY.gpg" | gpg --dearmor | tee /etc/apt/trusted.gpg.d/atlas.gpg > /dev/null 2>&1
 curl -s --compressed -o /etc/apt/sources.list.d/atlas.list "https://packages.atlas.bi/deb/atlas.list"
 apt update
